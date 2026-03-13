ab08747215a468d4a98b47e7fc294ae111f8a47f
Mudasra <km4446245@gmail.com>
2026-03-13 12:17:22 +0500

    Redux, Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './pages/LandingPage'
+import ThankYouPage from './pages/ThankYouPage'
+import Layout from './pages/Layout'
 
 function App() {
 
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Routes>
+        <Route element={<Layout />}>
+        <Route path='/' element={<LandingPage />}/>
+        <Route path='/thankYou' element={<ThankYouPage />}/>
+        </Route>
+      </Routes>
   )
 }
 
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../sections/Navbar/Navbar'
+
+const LandingPage = () => {
+  return (
+    <>
+    <main>
+
+    </main>
+    </>
+  )
+}
+
+export default LandingPage
